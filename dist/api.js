a11578aafbcef49e756c59abf1d1cf237afc2b8b
Avnkarthik <annavarapukarthik0@gmail.com>
2025-07-13 14:50:11 +0530

    dashboard error corrections

--- dist/api.js
+++ dist/api.js
@@ -238,10 +238,12 @@ const dashboard = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                 req.session.user.name = [familyName, givenName].filter(Boolean).join(" ");
             }
         }
+        console.log("before save");
         req.session.save((err) => {
   if (err) console.error("Session save error:", err);
   console.log("Session saved successfully.");
 });
+        
         console.log("name:", req.session.user.name);
         yield (0, database_1.dbconnection)();
         const filter = { email: userData.email };
