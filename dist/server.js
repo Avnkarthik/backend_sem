268c87e18adb4a6feffb1ebf1286a8f4e975a5ec
Avnkarthik <annavarapukarthik0@gmail.com>
2025-07-13 23:03:35 +0530

    added proxy line

--- dist/server.js
+++ dist/server.js
@@ -17,6 +17,7 @@ const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.set('trust proxy', 1); 
 app.use(body_parser_1.default.json());
 app.use(express_1.default.static(path_1.default.join(__dirname, 'client')));
 dotenv_1.default.config();
